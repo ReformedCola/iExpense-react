71554720201d1789ed882be2ee170b8d6ceb8578
ReformedCola <reformedcola@gmail.com>
2020-05-24 21:29:05 -0700

    Finished NotesSection UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,11 +19,30 @@ const TagsSection = styled.section`
 `;
 
 const NotesSection = styled.section`
-
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `;
+
 const CategorySection = styled.section`
 
 `;
+
 const NumberPadSection = styled.section`
 
 `;
@@ -44,7 +63,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>Notes:</span>
-          <input type="text"/>
+          <input type="text" placeholder="Tap to add your notes here ~"/>
         </label>
       </NotesSection>
       <CategorySection>
