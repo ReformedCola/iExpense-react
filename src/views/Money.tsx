4851a91b1d24c96d308e4ef436aa06f4197cf6ea
ReformedCola <reformedcola@gmail.com>
2020-05-24 22:27:38 -0700

    Finished NumberPadSection UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -34,32 +34,76 @@ const NotesSection = styled.section`
 
 const CategorySection = styled.section`
   font-size: 24px;
-  > ul {
-    display: flex;
-    background: #c4c4c4;
+  > ul { display: flex; background: #c4c4c4;
     > li {
-      width: 50%;
-      text-align: center;
-      padding: 16px 0;
-      position: relative;
+      width: 50%; text-align: center;
+      padding: 16px 0; position: relative;
       &.selected::after {
-        content: '';
-        display: block;
-        height: 3px;
-        background: #333;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        left: 0;
+        content: ''; display: block; height: 3px;
+        background: #333; position: absolute;
+        bottom: 0; width: 100%; left: 0;
       }
     }
   }
 `;
 
 const NumberPadSection = styled.section`
-
+  display: flex; 
+  flex-direction: column;
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+                inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+      &.zero {
+        width: 50%;
+      }
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #e0e0e0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: #d3d3d3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: #c1c1c1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: #b8b8b8;
+      }
+      &:nth-child(12) {
+        background: #9a9a9a;
+      }
+      &:nth-child(14) {
+        background: #a9a9a9;
+      }
+    }
+  }
 `;
-
 
 function Money() {
   return (
@@ -86,10 +130,10 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>
+        <div className="output">
           100
         </div>
-        <div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -101,8 +145,8 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
