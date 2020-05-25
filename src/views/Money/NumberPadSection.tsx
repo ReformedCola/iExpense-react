0e2ede6c4cc23963e6d1b8fbfed321b5496f8b1f
ReformedCola <reformedcola@gmail.com>
2020-05-25 16:12:12 -0700

    Remodelled NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,99 +1,26 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-  display: flex; flex-direction: column;
-  > .output {
-    background: white; font-size: 36px; line-height: 72px;
-    text-align: right; padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-                inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-  > .pad {
-    > button {
-      font-size: 18px; float: left; width: 25%;
-      height: 64px; border: none;
-      &.ok { height: 128px; float: right; }
-      &.zero { width: 50%; }
-      &:nth-child(1) { background: #f2f2f2; }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #e0e0e0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #d3d3d3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #c1c1c1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #b8b8b8;
-      }
-      &:nth-child(12) {
-        background: #9a9a9a;
-      }
-      &:nth-child(14) {
-        background: #a9a9a9;
-      }
-    }
-  }
-`;
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
   const setOutput = (output: string) => {
     if (output.length > 16) {
       output = output.slice(0, 16);
-    } else if(output.length === 0) {
+    } else if (output.length === 0) {
       output = '0';
     }
     _setOutput(output);
-  }
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.indexOf('.') >= 0) {
-          return
-        }
-        setOutput(output + '.')
-        break;
-      case 'Del':
-        if (output.length === 1) {
-          setOutput('');
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case 'AC':
-        setOutput('');
-        break;
-      case 'OK':
-        console.log('OK');
-        break;
+    if (text === 'OK') {
+      // TODO
+      return;
+    }
+    if ('0123456789.'.split('').concat(['Del', 'AC']).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output));
     }
   };
   return (
