d77783ec88bba25ff3ae3a3d3da341d3b436bdf1
ReformedCola <reformedcola@gmail.com>
2020-05-24 21:45:55 -0700

    Finished CategorySection UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -19,28 +19,41 @@ const TagsSection = styled.section`
 `;
 
 const NotesSection = styled.section`
-  background: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
+  background: #f5f5f5; padding: 0 16px; font-size: 14px;
   > label {
-    display: flex;
-    align-items: center;
+    display: flex; align-items: center;
     > span {
-      margin-right: 16px;
-      white-space: nowrap;
+      margin-right: 16px; white-space: nowrap;
     }
     > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
+      display: block; width: 100%; height: 72px;
+      background: none; border: none;
     }
   }
 `;
 
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
 `;
 
 const NumberPadSection = styled.section`
@@ -68,7 +81,7 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>Expense</li>
+          <li className="selected">Expense</li>
           <li>Income</li>
         </ul>
       </CategorySection>
