c175576ab781f157ed2d1fc6a08cffe6ac922e74
ReformedCola <reformedcola@gmail.com>
2020-05-25 15:47:38 -0700

    Finished functions of buttons on NumberPadSection

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -46,7 +46,15 @@ const Wrapper = styled.section`
 `;
 
 const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState('0');
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if(output.length === 0) {
+      output = '0';
+    }
+    _setOutput(output);
+  }
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
@@ -68,13 +76,20 @@ const NumberPadSection: React.FC = () => {
         }
         break;
       case '.':
-        console.log('.');
+        if (output.indexOf('.') >= 0) {
+          return
+        }
+        setOutput(output + '.')
         break;
       case 'Del':
-        console.log('Del');
+        if (output.length === 1) {
+          setOutput('');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case 'AC':
-        console.log('AC');
+        setOutput('');
         break;
       case 'OK':
         console.log('OK');
