33e9e7a8113ba6be25aa8c48a40dcee3355aca06
ReformedCola <reformedcola@gmail.com>
2020-05-26 10:33:47 -0700

    Created router of /tags/:tag, and used exact

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {useTags} from 'useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -11,11 +12,13 @@ const TagList = styled.ol`
     //#e5e5e7
     border-bottom: 1px solid #d5d5d9;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
     margin-left: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    > a {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 16px 12px 0;
+    }
   }
 `;
 
@@ -37,7 +40,7 @@ const Center = styled.div`
 
 const Space = styled.div`
   height: 16px;
-`
+`;
 
 function Tags() {
   const {tags, setTags} = useTags();
@@ -46,8 +49,10 @@ function Tags() {
       <TagList>
         {tags.map(tag =>
           <li key={tag}>
-            <span className="oneLine">{tag}</span>
-            <Icon name="right"/>
+            <Link to={'/tags/' + tag}>
+              <span className="oneLine">{tag}</span>
+              <Icon name="right"/>
+            </Link>
           </li>
         )}
       </TagList>
