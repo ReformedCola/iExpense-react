eab0e33854ce3c208fdbc1f6c6953223c6005c15
ReformedCola <reformedcola@gmail.com>
2020-05-26 15:10:39 -0700

    Created createId.tsx

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,10 +1,11 @@
 import {useState} from 'react';
+import {createId} from 'lib/createId';
 
 const useTags = () => {
   const [tags, setTags] = useState<{id: number; name: string}[]>([
-    {id: 1, name: 'Cloth'},
-    {id: 2, name: 'Food'},
-    {id: 3, name: 'Travel'},
+    {id: createId(), name: 'Cloth'},
+    {id: createId(), name: 'Food'},
+    {id: createId(), name: 'Travel'},
   ]);
   return {tags, setTags}
 };
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'useTags';
+import {createId} from 'lib/createId';
 
 const Wrapper = styled.section`
   background: #ffffff; padding: 12px 16px; 
@@ -34,7 +35,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('Enter the name of the tag:');
     if (tagName !== null) {
-      setTags([...tags, {id: Math.random(), name: tagName}]);
+      setTags([...tags, {id: createId(), name: tagName}]);
     }
   };
   const onToggleTag = (tagId: number) => {
