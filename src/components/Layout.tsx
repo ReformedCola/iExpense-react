d74345f762b997a147eab4502b87ffa4ea57eb42
ReformedCola <reformedcola@gmail.com>
2020-05-27 20:59:08 -0700

    Added auto scroll for smaller phones

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Nav from './Nav';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -13,15 +13,33 @@ const Main = styled.div`
   overflow: auto;
 `;
 
-const Layout = (props: any) => {
+type Props = {
+  className?: string;
+  scrollTop?: number;
+}
+
+const Layout: React.FC<Props> = (props) => {
+  const mainRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setTimeout(() => {
+      if (!mainRef.current) {
+        return;
+      }
+      mainRef.current.scrollTop = props.scrollTop!;
+    }, 0);
+  }, [props.scrollTop]);
   return (
     <Wrapper>
-      <Main className={props.className}>
+      <Main ref={mainRef} className={props.className}>
         {props.children}
       </Main>
       <Nav/>
     </Wrapper>
   );
+};
+
+Layout.defaultProps = {
+  scrollTop: 0
 };
 
 export default Layout;
