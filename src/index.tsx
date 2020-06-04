b7613a6b913841a29a86696744deff9753502561
ReformedCola <reformedcola@gmail.com>
2020-06-04 14:33:57 -0700

    Modified global scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import 'index.scss';
+import App from 'App';
+import 'styles/index.scss';
 
 ReactDOM.render(
-    <App />,
+  <App/>,
   document.getElementById('root')
 );
