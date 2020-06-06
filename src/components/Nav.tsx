cebc623aba21bbd30e60d229e91f36cc8feb3ff0
ReformedCola <reformedcola@gmail.com>
2020-06-06 16:21:12 -0700

    Remodelled Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,61 +1,68 @@
 import styled from 'styled-components';
-import {NavLink} from 'react-router-dom';
-import React from 'react';
-import Icon from './Icon';
+import {Link, useLocation} from 'react-router-dom';
+import * as React from 'react';
+import Icon from './NewIcon';
+import theme from '../theme';
+
+type TNavItem = {
+  selected: boolean
+}
 
 const NavWrapper = styled.nav`
+  padding: 12px 0;
   background: white;
-  line-height: 24px;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-  > ul {
-    display: flex;
-    > li {
-      width: 33.3333%;
-      text-align: center;
-      > a {
-        display: flex;
-        flex-direction: column;
-        padding: 4px 0;
-        justify-content: center;
-        align-items: center;
-        .icon {
-          width: 24px;
-          height: 24px;
-        }
-        &.selected {
-          color: #00a0e9;
-          .icon {
-            fill: #00a0e9;
-          }
-        }
-      }
-    }
+  box-shadow: 0px -2px 12px 0px rgba(192, 196, 204, 0.4);
+  z-index: 2;
+  a {
+    text-decoration: none;
+  }
+  .selected {
+    color: ${props => props.theme.$success}
   }
 `;
 
-const Nav = () => {
+const NavItem = styled(Link)<TNavItem>`
+  flex-grow: 1;
+  width: 33.333%;
+  float: left;
+  color: ${props => props.theme.$normalText};
+  font-size: ${props => props.theme.$normalTextSize};
+  text-align: center;
+  > div {
+    margin-top: 4px;
+    color: ${props => props.selected ? props.theme.$success : props.theme.$normalText};
+    text-align: center;
+  }
+`;
+
+const Nav: React.FC = () => {
+  const {pathname} = useLocation();
   return (
-    <NavWrapper>
-      <ul>
-        <li>
-          <NavLink to="/tags" activeClassName="selected">
-            <Icon name="details"/>
-            Tags
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/money" activeClassName="selected">
-            <Icon name="add"/>
-            Expense
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/statistics" activeClassName="selected">
-            <Icon name="statistics"/>
-            Statistics
-          </NavLink>
-        </li>
-      </ul>
+    <NavWrapper className="clearfix">
+      <NavItem to="/summary" selected={pathname === '/summary'}>
+        {
+          pathname === '/summary' ?
+            <Icon name="solid-order" size={24} color={theme.$success}/> :
+            <Icon name="order" size={24}/>
+        }
+        <div>Summary</div>
+      </NavItem>
+      <NavItem to="/chart" selected={pathname === '/analysis'}>
+        {
+          pathname === '/chart' ?
+            <Icon name="solid-chart" size={24} color={theme.$success}/> :
+            <Icon name="chart" size={24}/>
+        }
+        <div>Chart</div>
+      </NavItem>
+      <NavItem to="/settings" selected={pathname === 'settings'}>
+        {
+          pathname === '/settings' ?
+            <Icon name="solid-settings" size={24} color={theme.$success}/> :
+            <Icon name="settings" size={24}/>
+        }
+        <div>Setting</div>
+      </NavItem>
     </NavWrapper>
   );
 };
